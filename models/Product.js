f34d8aaad53e06b50b976e21c530fc16602e8cc5
Ben Parks <benparks93@gmail.com>
2024-06-04 15:36:02 -0500

    Set up every model logic besides the index.js

--- models/Product.js
+++ models/Product.js
@@ -5,11 +5,54 @@ const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
-
+const Category = require('./Category')
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: {
+          args: true,
+          msg: 'price must be a decimal number with 2 after the decimal'
+        },
+        min: {
+          args: [0],
+          msg: 'price must be greater than free.'
+        }
+
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: {
+          args: true,
+          msg: 'stock needs to be a number.'
+        }
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Category,
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
